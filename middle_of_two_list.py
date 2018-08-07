0f80f7758857aa4104139ad42fa6ab33475b61cd
Yiling-J <njjyl723@gmail.com>
2018-08-07 23:00:25 +0800

    Fix

--- middle_of_two_list.py
+++ middle_of_two_list.py
@@ -10,20 +10,26 @@ def get_middle(l1, l2):
     if mid1 > mid2:
         left = l2[len(l2) / 2 + len(l2) % 2 - 1:]
         right = l1[:len(l1) / 2 + 1]
+        q = 1 if len2 % 2 else 2
     elif mid1 < mid2:
         left = l1[len(l1) / 2 + len(l1) % 2 - 1:]
         right = l2[:len(l2) / 2 + 1]
+        q = 1 if len1 % 2 else 2
     else:
         return mid1
-    return get_same_distance(left, right, need)
+    return get_same_distance(left, right, need, q)
 
 
-def get_same_distance(left, right, need):
+def get_same_distance(left, right, need, q):
     lc = len(left)
     rc = len(right)
     while len(left) > 2 or len(right) > 2:
         print(left)
         print(right)
+        if len(left) == 1:
+            return right[-1]
+        if len(right) == 1:
+            return left[0]
         l_length = len(left)
         r_length = len(right)
         n = min(l_length, r_length) / 2
@@ -46,10 +52,7 @@ def get_same_distance(left, right, need):
         all = left + right
         all.sort()
         if need == 1:
-            if lc % 2:
-                return all[2]
-            else:
-                return all[1]
+            return all[q]
         else:
             return all[1:3]
 
@@ -59,8 +62,8 @@ l1 = [randint(0, 50) for i in range(randint(2, 20))]
 l2 = [randint(0, 50) for i in range(randint(2, 20))]
 
 # fix length test
-# l1 = [randint(0, 50) for i in range(2)]
-# l2 = [randint(0, 50) for i in range(7)]
+l1 = [randint(0, 50) for i in range(2)]
+l2 = [randint(0, 50) for i in range(15)]
 
 # error
 # l1 = [2, 8]
@@ -86,9 +89,11 @@ all.sort()
 print(all)
 l = len(all)
 if l % 2: 
-	print(all[l/2])
+        t = all[l/2]
 else:
-	print(all[l/2-1], all[l/2])
+        t = all[l/2-1], all[l/2]
+print(t)
+print(r == t)
 
 
 """

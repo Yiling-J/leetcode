79f1e0057c85ce53119916488ebcbbbbc92f0388
Yiling-J <njjyl723@gmail.com>
2018-08-11 17:08:03 +0800

    First right version

--- middle_of_two_list.py
+++ middle_of_two_list.py
@@ -1,9 +1,110 @@
 from random import randint
 
+class Solution(object):
+    def get_middle(self, l1, l2):
+        len1 = len(l1)
+        len2 = len(l2)
+        r = False
+        if len1 % 2 and len1 < len2:
+            r = True
+        elif len2 % 2 and len2 < len1:
+            r = True
+        need = 2 - (len(l1) + len(l2)) % 2
+        mid1 = l1[len1 / 2] if len1 % 2 == 1 else (l1[len1 / 2 - 1] + l1[len1 / 2]) / 2.0
+        mid2 = l2[len2 / 2] if len2 % 2 == 1 else (l2[len2 / 2 - 1] + l2[len2 / 2]) / 2.0
+        if mid1 > mid2:
+            left = l2[len(l2) / 2 + len(l2) % 2 - 1:]
+            right = l1[:len(l1) / 2 + 1]
+            q = 1 if len2 % 2 else 2
+        elif mid1 < mid2:
+            left = l1[len(l1) / 2 + len(l1) % 2 - 1:]
+            right = l2[:len(l2) / 2 + 1]
+            q = 1 if len1 % 2 else 2
+        else:
+            return mid1
+        return self.get_same_distance(left, right, need, q, r)
+
+
+    def get_same_distance(self, left, right, need, q, r):
+        lc = len(left)
+        rc = len(right)
+        while len(left) > 2 or len(right) > 2 or len(left) == 1 or len(right) == 1:
+            if len(left) == 1:
+                # need 2, reutn bigger 2 of left one and right last two
+                if need == 1:
+                    if not r:
+                        return right[-1]
+                    else:
+                        all = [left[0], right[-1], right[-2]]
+                        all.sort()
+                        return all[1]
+                else:
+                    all = [left[0], right[-1], right[-2]]
+                    all.sort()
+                    return sum(all[1:]) / 2.0
+            if len(right) == 1:
+                # need 2, return smaller 2 of right and left first two
+                if need == 1:
+                    if not r:
+                        return left[0]
+                    else:
+                        all = [right[0], left[0], left[1]]
+                        all.sort()
+                        return all[1]
+                else:
+                    all = [right[0], left[0], left[1]]
+                    all.sort()
+                    return sum(all[:2]) / 2.0
+            l_length = len(left)
+            r_length = len(right)
+            n = min(l_length, r_length) / 2
+            l_value = left[n]
+            r_value = right[-n - 1]
+            if l_value == r_value:
+                return l_value
+            if l_value > r_value:
+                left = left[:n + 1]
+                right = right[-n - 1:]
+            else:
+                left = left[n:]
+                right = right[:-n]
+        if len(left) > 1 or len(right) > 1:
+            all = left + right
+            all.sort()
+            if need == 1:
+                return all[q]
+            else:
+                return sum(all[1:3]) / 2.0
+
+    def middle_of_list(self, l):
+        len_l = len(l)
+        if len_l % 2:
+            return l[len_l / 2]
+        else:
+            return sum([l[len_l / 2 - 1], l[len_l / 2]]) / 2.0
+            
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        if not nums1 or not nums2:
+            return self.middle_of_list(nums1 or nums2)
+        if len(nums1) == len(nums2) == 1:
+            return (nums1[0] + nums2[0]) / 2.0
+        return self.get_middle(nums1, nums2)
+        
+
 
 def get_middle(l1, l2):
     len1 = len(l1)
     len2 = len(l2)
+    r = False
+    if len1 % 2 and len1 < len2:
+        r = True
+    elif len2 % 2 and len2 < len1:
+        r = True
     need = 2 - (len(l1) + len(l2)) % 2
     mid1 = l1[len1 / 2] if len1 % 2 == 1 else (l1[len1 / 2 - 1] + l1[len1 / 2]) / 2.0
     mid2 = l2[len2 / 2] if len2 % 2 == 1 else (l2[len2 / 2 - 1] + l2[len2 / 2]) / 2.0
@@ -16,20 +117,23 @@ def get_middle(l1, l2):
         right = l2[:len(l2) / 2 + 1]
         q = 1 if len1 % 2 else 2
     else:
-        return mid1
-    return get_same_distance(left, right, need, q)
+        return mid1 if need == 1 else [mid1, mid1]
+    return get_same_distance(left, right, need, q, r)
 
 
-def get_same_distance(left, right, need, q):
+def get_same_distance(left, right, need, q, r):
     lc = len(left)
     rc = len(right)
-    while len(left) > 2 or len(right) > 2:
-        print(left)
-        print(right)
+    while len(left) > 2 or len(right) > 2 or len(left) == 1 or len(right) == 1:
         if len(left) == 1:
             # need 2, reutn bigger 2 of left one and right last two
             if need == 1:
-                return right[-1]
+                if not r:
+                    return right[-1]
+                else:
+                    all = [left[0], right[-1], right[-2]]
+                    all.sort()
+                    return all[1]
             else:
                 all = [left[0], right[-1], right[-2]]
                 all.sort()
@@ -37,7 +141,12 @@ def get_same_distance(left, right, need, q):
         if len(right) == 1:
             # need 2, return smaller 2 of right and left first two
             if need == 1:
-                return left[0]
+                if not r:
+                    return left[0]
+                else:
+                    all = [right[0], left[0], left[1]]
+                    all.sort()
+                    return all[1]
             else:
                 all = [right[0], left[0], left[1]]
                 all.sort()
@@ -48,8 +157,6 @@ def get_same_distance(left, right, need, q):
         l_value = left[n]
         r_value = right[-n - 1]
         if l_value == r_value:
-            print('---F---')
-            print(l_value)
             return l_value if need == 1 else [l_value, l_value]
         if l_value > r_value:
             left = left[:n + 1]
@@ -57,10 +164,7 @@ def get_same_distance(left, right, need, q):
         else:
             left = left[n:]
             right = right[:-n]
-    print('---F---')
-    print(left)
-    print(right)
-    if len(left) == len(right) == 2:
+    if len(left) > 1 or len(right) > 1:
         all = left + right
         all.sort()
         if need == 1:
@@ -70,16 +174,10 @@ def get_same_distance(left, right, need, q):
 
 
 def test():
-    l1 = [randint(0, 50) for i in range(randint(2, 20))]
-    l2 = [randint(0, 50) for i in range(randint(2, 20))]
-
-    # fix length test
-    l1 = [randint(0, 50) for i in range(3)]
-    l2 = [randint(0, 50) for i in range(8)]
-
-    # error
-    # l1 = [2, 8]
-    # l2 = [4, 6, 15, 17, 22, 29, 33, 42, 42, 49]
+    l1 = [randint(0, 50) for i in range(randint(0, 50))]
+    l2 = [randint(0, 50) for i in range(randint(0, 50))]
+    if l1 == l2 == []:
+    	l1 = [1]
 
     l1.sort()
     l2.sort()
@@ -92,7 +190,8 @@ def test():
     print('')
 
 
-    r = get_middle(l1, l2)
+    s = Solution()
+    r = s.findMedianSortedArrays(l1, l2)
     print('===Guess===')
     print(r)
     print('===Merged===')
@@ -103,12 +202,12 @@ def test():
     if l % 2: 
         t = all[l/2]
     else:
-        t = [all[l/2-1], all[l/2]]
+        t = sum([all[l/2-1], all[l/2]]) / 2.0
     print(t)
     return r == t
 
-for i in range(1000):
+for i in range(5000):
     print(i)
     result = test()
     if not result:
-        break
+        raise Exception('===Unmatch!===')

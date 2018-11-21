436f5221bb0063b676cb86bcc8778d8f6ce3762c
Yiling-J <njjyl723@gmail.com>
2018-11-21 22:12:48 +0800

    Update doc

--- 132_palin_partition2.py
+++ 132_palin_partition2.py
@@ -1,7 +1,27 @@
 """
-TODO: Add doc.
+First, we use DP to solve this.
+
+Then the question is, at current word n, if min cut is t[n], how do
+we use previous solutions, t[0...n-1] to get current t[n]?
+
+We have two options at n:
+
+1. Current word s[n] is not in a palindrome, this is simple, because we
+have to cut here, and t[n] is t[n-1] + 1.
+
+2. Current word s[n] is in a palindrome, then if palindrome start at j,
+we can know t[n] is t[j] + 1, because j to n is a palindrome. And one
+important thing is that, n can be in multi palindromes, so we need to
+try one by one. To make this possible, we also need to remember all
+possible palindrome. Here I use two set p1 and p2 to save the palindrome mid point,
+p1 for odd palindrome, for example "adda", I save 'd'. And p2 for even palindrome,
+for example "ada", I save 'd'(maybe I can combine thesee two). At each n,
+we loop p1 and p2, if palindrome invalid at n, just delete it from set.
+
+Finally, when we get all possible t[n], we compare and find the minium one.
 
 Faster than 88%, but still much slower than that crazy 24ms solution...
+
 """
 
 class Solution(object):
